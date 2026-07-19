244ff0515b4a079ef82e8500c752c34eff0ba201
s0416 <2500949756@qq.com>
2026-07-19 08:48:51 +0800

    去掉预置mcp服务

--- app/src/main/java/me/rerere/rikkahub/ui/pages/setting/components/McpPresets.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/setting/components/McpPresets.kt
@@ -15,14 +15,7 @@ enum class McpPresetType {
     STREAMABLE_HTTP
 }
 
-val MCP_PRESETS = listOf(
-    McpPreset(
-        name = "4399",
-        url = "https://toy.cedarstar.org",
-        type = McpPresetType.STREAMABLE_HTTP,
-        description = "作者:小红书@南山君"
-    )
-)
+val MCP_PRESETS = emptyList<McpPreset>()
 
 fun McpPreset.toMcpServerConfig(): McpServerConfig {
     val commonOptions = McpCommonOptions(name = this.name)
