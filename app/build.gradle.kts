0683ba11587aacd6f563b6bd0dff450db568ff33
s0416 <2500949756@qq.com>
2026-08-16 14:06:21 +0800

    451版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "ailand.lastchat.rikkafork.cocolal"
         minSdk = 28
         targetSdk = 36
-        versionCode = 25
-        versionName = "4.5.0.1"
+        versionCode = 26
+        versionName = "4.5.1"
 
         buildConfigField("String", "GITHUB_REPO", "\"dragonstaryuri-sys/evolia\"")
 
